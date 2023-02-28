9d2dba03bf8b7a5ed04c9fc475b1c11fa2db4cb9
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-02-28 01:40:59 -0300

    cors

--- api/app.js
+++ api/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./src/routes/index.js");
 const app = express();
-
+app.use(
+  cors({
+    origin: "*",
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use("/api", routes);
 
 module.exports = app;
