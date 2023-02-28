5475d273c7f0e7aef5ffd9318543247dd43839e1
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-02-28 01:45:57 -0300

    error

--- api/app.js
+++ api/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const routes = require("./src/routes/index.js");
 const app = express();
+
+app.use(express.json());
+
 app.use(
   cors({
     origin: "*",
@@ -10,5 +13,12 @@ app.use(
   })
 );
 app.use("/api", routes);
+
+app.use((err, req, res, next) => {
+  let error = {};
+  error.msg = err.msg || err;
+  error.status = err.status || 500;
+  res.status.send(error);
+});
 
 module.exports = app;
