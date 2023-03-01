23c32069c3f8433846ae02b3091b7a565968e597
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-03-01 15:55:28 -0300

    Logo. SVG a componente de ReactJS

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { GlobalStyle } from "./GlobalStyles";
 import { Home } from "./views/Home";
+import { Logo } from './components/Logo';
 
 function App() {
   return (
     <div>
       <GlobalStyle />
+      <Logo />
       {/* <ListOfCategories /> */}
       <Home />
     </div>
