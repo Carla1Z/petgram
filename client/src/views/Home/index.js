382e9a2d86af47e8fa75d24b76a0820e852fc3ad
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-03-01 14:56:14 -0300

    home

--- client/src/views/Home/index.js
+++ client/src/views/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { ListOfCategories } from '../components/ListOfCategories/index.js'
-import {getCategories} from '../redux/actions.js'
+import { ListOfCategories } from '../../components/ListOfCategories/index.js'
+import {getCategories} from '../../redux/actions.js'
 import {useDispatch, useSelector} from 'react-redux'
 import { useEffect } from "react"
 
