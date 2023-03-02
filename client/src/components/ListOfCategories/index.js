237ac1d2218301d2bc2be5cbca00f1257f91e168
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-03-02 02:40:39 -0300

    implementando hooks para traerme categories!

--- client/src/components/ListOfCategories/index.js
+++ client/src/components/ListOfCategories/index.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Category } from "../Category";
 import { List, Item } from "./styles";
 
-export const ListOfCategories = ({allCategories}) => {
+export const ListOfCategories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(function () {
+    fetch("http://localhost:3001/categories")
+      .then((res) => res.json())
+      .then((response) => {
+        setCategories(response);
+      });
+  }, []);
+
   return (
     <List>
-      {
-      allCategories.map((category) => (
+      {categories.map((category) => (
         <Item key={category.id}>
           <Category {...category} />
         </Item>
-      ))
-      }
+      ))}
     </List>
   );
 };
--- client/src/views/Home/index.js
+++ client/src/views/Home/index.js
@@ -1,21 +1,11 @@
 import React from "react";
 import { ListOfCategories } from "../../components/ListOfCategories/index.js";
 import { ListOfPhotoCard } from "../../components/ListOfPhotoCard/index.js";
-import { getCategories } from "../../redux/actions.js";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
 
 export const Home = () => {
-  const dispatch = useDispatch();
-  const allCategories = useSelector((state) => state.categories);
-
-  useEffect(() => {
-    dispatch(getCategories());
-  }, [dispatch]);
-
   return (
     <div>
-      <ListOfCategories allCategories={allCategories} />
+      <ListOfCategories />
       <ListOfPhotoCard />
     </div>
   );
