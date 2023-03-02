a64291140f54155eaf59aece6076821e9ba2333e
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-03-02 02:55:26 -0300

    useEffect para mostrar las categorias al hacer scroll

--- client/src/components/ListOfCategories/index.js
+++ client/src/components/ListOfCategories/index.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { Category } from "../Category";
 import { List, Item } from "./styles";
 
 export const ListOfCategories = () => {
   const [categories, setCategories] = useState([]);
+  const [showFixed, setShowFixed] = useState(false);
 
   useEffect(function () {
     fetch("http://localhost:3001/categories")
@@ -13,13 +14,28 @@ export const ListOfCategories = () => {
       });
   }, []);
 
-  return (
-    <List>
+  useEffect(function () {
+    const onScroll = (e) => {
+      const newShowFixed = window.scrollY > 200;
+      showFixed !== newShowFixed && setShowFixed(newShowFixed);
+    };
+    document.addEventListener("scroll", onScroll);
+  });
+
+  const renderList = (fixed) => (
+    <List className={fixed ? "fixed" : ""}>
       {categories.map((category) => (
         <Item key={category.id}>
           <Category {...category} />
         </Item>
       ))}
     </List>
+  );
+
+  return (
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
   );
 };
